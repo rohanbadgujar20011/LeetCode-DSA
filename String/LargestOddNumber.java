fb8b73712f9a770dae1d3ab9f7625f27b4f27c2f
rohanbadgujar20011 <rohanbadgujar2001@gmail.com>
2025-10-29 17:20:15 +0530

    Largest Odd Number

---
+++ String/LargestOddNumber.java
@@ -0,0 +1,20 @@
+package String;
+
+public class LargestOddNumber {
+    public static String largestOddNumber(String num) {
+        int n = num.length();
+        for (int i = n - 1; i >= 0; i--) {
+            int digit = num.charAt(i) - '0';
+            if (digit % 2 == 1) {
+                return num.substring(0, i + 1);
+            }
+        }
+        return "";
+    }
+
+
+
+    public static void main(String[] args) {
+        System.out.println(largestOddNumber("4206"));
+    }
+}
