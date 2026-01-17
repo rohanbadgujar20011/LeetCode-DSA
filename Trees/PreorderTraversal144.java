5133eb9f570d5c6488da1f74e20bf4e254af55ee
rohanbadgujar20011 <rohanbadgujar2001@gmail.com>
2026-01-17 14:15:55 +0530

    Same Tree

--- Trees/PreorderTraversal144.java
+++ Trees/PreorderTraversal144.java
@@ -4,19 +4,6 @@ package Trees;
 import java.util.ArrayList;
 import java.util.List;
 
-class TreeNode {
-      int val;
-      TreeNode left;
-      TreeNode right;
-      TreeNode() {}
-      TreeNode(int val) { this.val = val; }
-      TreeNode(int val, TreeNode left, TreeNode right) {
-          this.val = val;
-          this.left = left;
-          this.right = right;
-      }
-  }
-
 public class PreorderTraversal144 {
 
     public List<Integer> preorderTraversal(TreeNode root) {
